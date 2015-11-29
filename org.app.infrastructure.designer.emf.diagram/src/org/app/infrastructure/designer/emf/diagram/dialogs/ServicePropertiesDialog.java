cacef2ad5008a6c7585aec9eeff7682e4bdce3ca
Rubén de Dios <rdediosb@gmail.com>
2015-11-29 23:46:39 +0100

    libs updated

--- org.app.infrastructure.designer.emf.diagram/src/org/app/infrastructure/designer/emf/diagram/dialogs/ServicePropertiesDialog.java
+++ org.app.infrastructure.designer.emf.diagram/src/org/app/infrastructure/designer/emf/diagram/dialogs/ServicePropertiesDialog.java
@@ -1,23 +1,29 @@
 package org.app.infrastructure.designer.emf.diagram.dialogs;
 
 import java.net.URI;
-import java.util.Collection;
-import java.util.Map;
-import java.util.Set;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriBuilder;
 
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.Text;
 import org.glassfish.jersey.client.ClientConfig;
 
 import containers.Service;
@@ -25,6 +31,8 @@ import containers.Service;
 public class ServicePropertiesDialog extends Dialog{
 
 	private Service service;
+	private Text text;
+	private Table table;
 	
 	public ServicePropertiesDialog(Shell parentShell, Service srv) {
 		super(parentShell);
@@ -33,112 +41,87 @@ public class ServicePropertiesDialog extends Dialog{
 
 	@Override
 	protected Control createDialogArea(Composite parent) {
+
+		Composite area = (Composite) super.createDialogArea(parent);
+		Composite container = new Composite(area, SWT.NONE);
+		FillLayout fl_container = new FillLayout(SWT.HORIZONTAL);
+		fl_container.marginHeight = 10;
+		fl_container.marginWidth = 10;
+		container.setLayout(fl_container);
+		GridData gd_container = new GridData(GridData.FILL_BOTH);
+		gd_container.grabExcessHorizontalSpace = false;
+		container.setLayoutData(gd_container);
 		
-		Composite container = (Composite) super.createDialogArea(parent);
+		Group grpServiceConfiguration = new Group(container, SWT.NONE);
+		grpServiceConfiguration.setText("Select Docker Image");
 		
-//		ClientConfig config = new ClientConfig();
-	    Client client = ClientBuilder.newClient();
-	    
-		Button button = new Button(container, SWT.PUSH);
+		text = new Text(grpServiceConfiguration, SWT.BORDER);
+		text.setBounds(57, 15, 244, 19);
 		
-		button.setLayoutData(new GridData(SWT.BEGINNING, SWT.CENTER, false,
-				false));
-		button.setText(service.getName());
-		button.addSelectionListener(new SelectionAdapter() {
+		table = new Table(grpServiceConfiguration, SWT.BORDER | SWT.FULL_SELECTION);
+		table.setBounds(21, 47, 385, 129);
+		table.setHeaderVisible(true);
+		table.setLinesVisible(true);
+		
+		TableColumn tblclmnImage = new TableColumn(table, SWT.CENTER);
+		tblclmnImage.setWidth(100);
+		tblclmnImage.setText("Image");
+		
+		Button btnSearch = new Button(grpServiceConfiguration, SWT.NONE);
+		btnSearch.setBounds(307, 11, 69, 28);
+		btnSearch.setText("Search");
+
+		btnSearch.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
+
+				ClientConfig config = new ClientConfig();
+				Client client = ClientBuilder.newClient(config);
+
+//				URI uri = UriBuilder.fromUri("https://registry.hub.docker.com/v1/search?q="+text.getText()).build();
+				URI uri = UriBuilder.fromUri("https://registry.hub.docker.com/v1").build();
 				
-//				ClientConfig config = new ClientConfig();
-//			    Client client = null;
-//			    Configuration po = new Configuration(){
-//
-//					@Override
-//					public Set<Class<?>> getClasses() {
-//						// TODO Auto-generated method stub
-//						return null;
-//					}
-//
-//					@Override
-//					public Map<Class<?>, Integer> getContracts(Class<?> arg0) {
-//						// TODO Auto-generated method stub
-//						return null;
-//					}
-//
-//					@Override
-//					public Set<Object> getInstances() {
-//						// TODO Auto-generated method stub
-//						return null;
-//					}
-//
-//					@Override
-//					public Map<String, Object> getProperties() {
-//						// TODO Auto-generated method stub
-//						return null;
-//					}
-//
-//					@Override
-//					public Object getProperty(String arg0) {
-//						// TODO Auto-generated method stub
-//						return null;
-//					}
-//
-//					@Override
-//					public Collection<String> getPropertyNames() {
-//						// TODO Auto-generated method stub
-//						return null;
-//					}
-//
-//					@Override
-//					public RuntimeType getRuntimeType() {
-//						// TODO Auto-generated method stub
-//						return null;
-//					}
-//
-//					@Override
-//					public boolean isEnabled(Feature arg0) {
-//						// TODO Auto-generated method stub
-//						return false;
-//					}
-//
-//					@Override
-//					public boolean isEnabled(Class<? extends Feature> arg0) {
-//						// TODO Auto-generated method stub
-//						return false;
-//					}
-//
-//					@Override
-//					public boolean isRegistered(Object arg0) {
-//						// TODO Auto-generated method stub
-//						return false;
-//					}
-//
-//					@Override
-//					public boolean isRegistered(Class<?> arg0) {
-//						// TODO Auto-generated method stub
-//						return false;
-//					}
-//			    	
-//			    };
-//			    client = ClientBuilder.newClient(config);
-//			    
-//			    URI uri = UriBuilder.fromUri("https://registry.hub.docker.com").build();
-//			    
-//			    WebTarget target = client.target(uri);
-//			    
-//			    String plainAnswer = target.path("v1").request().accept(MediaType.TEXT_PLAIN).get(String.class);
-//			    
-				System.out.println("");
+				WebTarget target = client.target(uri);
+				
+				Object plainAnswer = target.path("search").queryParam("q", text.getText()).request().accept(MediaType.TEXT_PLAIN).get(String.class);    
+		        
+				System.out.println(plainAnswer);
 			}
 		});
 		
-		return parent;	
+		return area;
+		
+//		Composite container = (Composite) super.createDialogArea(parent);
+//
+//		Button button = new Button(container, SWT.PUSH);
+//
+//		button.setLayoutData(new GridData(SWT.BEGINNING, SWT.CENTER, false,
+//				false));
+//		button.setText(service.getName());
+//		button.addSelectionListener(new SelectionAdapter() {
+//			@Override
+//			public void widgetSelected(SelectionEvent e) {
+//
+//				ClientConfig config = new ClientConfig();
+//				Client client = ClientBuilder.newClient(config);
+//
+//				URI uri = UriBuilder.fromUri("https://registry.hub.docker.com").build();
+//
+//				WebTarget target = client.target(uri);
+//
+//				String plainAnswer = target.path("v1").request().accept(MediaType.TEXT_PLAIN).get(String.class);
+//
+//				System.out.println("");
+//			}
+//		});
+//
+//		return parent;	
 	}
-	
+
 	@Override
 	protected void createButtonsForButtonBar(Composite parent) {
 		createButton(parent, IDialogConstants.OK_ID, "Save", true);
-		createButton(parent, IDialogConstants.CANCEL_ID,
-				IDialogConstants.CANCEL_LABEL, false);
+		createButton(parent, IDialogConstants.CANCEL_ID, IDialogConstants.CANCEL_LABEL, false);
 	}
 
 	@Override
@@ -147,8 +130,8 @@ public class ServicePropertiesDialog extends Dialog{
 		newShell.setText(service.getName());
 	}
 
-//	  @Override
-//	  protected Point getInitialSize() {
-//	    return new Point(450, 300);
-//	  }
+	@Override
+	protected Point getInitialSize() {
+		return new Point(450, 375);
+	}
 }
