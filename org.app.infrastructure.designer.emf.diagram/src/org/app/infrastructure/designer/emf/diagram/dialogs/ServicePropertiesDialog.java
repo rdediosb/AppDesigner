2442082fa252b10e4fb79ccee26b497dd9c293dd
Rubén de Dios <rdediosb@gmail.com>
2015-12-01 00:35:15 +0100

    Added table columns

--- org.app.infrastructure.designer.emf.diagram/src/org/app/infrastructure/designer/emf/diagram/dialogs/ServicePropertiesDialog.java
+++ org.app.infrastructure.designer.emf.diagram/src/org/app/infrastructure/designer/emf/diagram/dialogs/ServicePropertiesDialog.java
@@ -10,9 +10,11 @@ import javax.ws.rs.core.UriBuilder;
 
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.dialogs.TitleAreaDialog;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
@@ -23,19 +25,24 @@ import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.TableItem;
 import org.eclipse.swt.widgets.Text;
 import org.glassfish.jersey.client.ClientConfig;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import containers.Service;
 
-public class ServicePropertiesDialog extends Dialog{
+public class ServicePropertiesDialog extends TitleAreaDialog{
 
 	private Service service;
 	private Text text;
 	private Table table;
+	Image imageOfficial;
 	
 	public ServicePropertiesDialog(Shell parentShell, Service srv) {
 		super(parentShell);
+		//Image imageOfficial = new Image(imageOfficial. .getDisplay(), ServicePropertiesDialog.class.getResourceAsStream("/icons/obj16/ContainersDiagramFile.gif") );
 		service = srv;
 	}
 
@@ -56,19 +63,32 @@ public class ServicePropertiesDialog extends Dialog{
 		grpServiceConfiguration.setText("Select Docker Image");
 		
 		text = new Text(grpServiceConfiguration, SWT.BORDER);
-		text.setBounds(57, 15, 244, 19);
+		text.setBounds(21, 15, 498, 19);
 		
-		table = new Table(grpServiceConfiguration, SWT.BORDER | SWT.FULL_SELECTION);
-		table.setBounds(21, 47, 385, 129);
+		table = new Table(grpServiceConfiguration, SWT.BORDER | SWT.FULL_SELECTION | SWT.VIRTUAL);
+		table.setBounds(21, 47, 573, 129);
 		table.setHeaderVisible(true);
 		table.setLinesVisible(true);
 		
-		TableColumn tblclmnImage = new TableColumn(table, SWT.CENTER);
-		tblclmnImage.setWidth(100);
-		tblclmnImage.setText("Image");
+		TableColumn columnName = new TableColumn(table, SWT.CENTER);
+		columnName.setWidth(140);
+		columnName.setText("Name");
+		
+		TableColumn columnDescription = new TableColumn(table, SWT.CENTER);
+		columnDescription.setWidth(361);
+		columnDescription.setText("Description");
+		
+		TableColumn columnOfficial = new TableColumn(table, SWT.CENTER);
+		columnOfficial.setWidth(25);
+		
+//		columnOfficial.setImage(image);
+		
+		TableColumn columnStars = new TableColumn(table, SWT.CENTER);
+		columnStars.setWidth(35);
+		columnStars.setText("Stars");
 		
 		Button btnSearch = new Button(grpServiceConfiguration, SWT.NONE);
-		btnSearch.setBounds(307, 11, 69, 28);
+		btnSearch.setBounds(525, 11, 69, 28);
 		btnSearch.setText("Search");
 
 		btnSearch.addSelectionListener(new SelectionAdapter() {
@@ -78,14 +98,37 @@ public class ServicePropertiesDialog extends Dialog{
 				ClientConfig config = new ClientConfig();
 				Client client = ClientBuilder.newClient(config);
 
-//				URI uri = UriBuilder.fromUri("https://registry.hub.docker.com/v1/search?q="+text.getText()).build();
 				URI uri = UriBuilder.fromUri("https://registry.hub.docker.com/v1").build();
 				
 				WebTarget target = client.target(uri);
 				
-				Object plainAnswer = target.path("search").queryParam("q", text.getText()).request().accept(MediaType.TEXT_PLAIN).get(String.class);    
+				
+				String plainAnswer = target.path("search").queryParam("q", text.getText()).request().accept(MediaType.TEXT_PLAIN).get(String.class);    
 		        
-				System.out.println(plainAnswer);
+				JSONObject obj = new JSONObject(plainAnswer);
+				JSONArray arr = obj.getJSONArray("results");
+				
+				if(arr.length()>0)
+				{
+					table.clearAll();
+				}
+				
+				for (int i = 0; i < arr.length(); i++)
+				{
+					JSONObject result = arr.getJSONObject(i);
+					String name = result.getString("name");
+					String description = result.getString("description");
+					boolean official = result.getBoolean("is_official");
+					int stars = result.getInt("star_count");
+					
+					TableItem item = new TableItem (table, SWT.NONE);
+					item.setText (0, name);
+					item.setText (1, description);
+					item.setText (2, "ok");
+					item.setText (3, "223");
+					
+				    System.out.println(name + "///" + description + "///" + official + "///" +stars);
+				}
 			}
 		});
 		
@@ -132,6 +175,6 @@ public class ServicePropertiesDialog extends Dialog{
 
 	@Override
 	protected Point getInitialSize() {
-		return new Point(450, 375);
+		return new Point(638, 375);
 	}
 }
